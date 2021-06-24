216ae74828a9e2a04dbc20b741f0f43f8425e045
Alyssa Antonaccio <alyantonaccio@gmail.com>
2021-06-24 10:58:23 -0500

    Forgot to save one file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-unused-vars */
 const express = require('express');
+
 const app = express();
-const db = require('./db/index.js');
 const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('./db/index.js');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Howdy from the other side');
