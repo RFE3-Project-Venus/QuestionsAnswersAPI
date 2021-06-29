3d9f4e2d85617a57ce77a51f70165444f8e75f58
Alyssa Antonaccio <alyantonaccio@gmail.com>
2021-06-28 19:46:59 -0500

    Fixed dependencies

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const express = require('express');
 
 const app = express();
 const bodyParser = require('body-parser');
-const path = require('path');
 const {
   getQuestionsAndAnswers,
   postQuestionsAndAnswers,
